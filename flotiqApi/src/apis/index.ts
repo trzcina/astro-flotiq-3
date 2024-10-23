bce8d1b5b7a0733d9502bf5d4e6a2f9bea58c7f4
Michał Chrząszcz <mchrzaszcz90@gmail.com>
2024-10-23 16:20:35 +0200

    Add Flotiq SDK installed via flotiq-api-ts in node modules

--- flotiqApi/src/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './BlogPostAPI';
-export * from './MediaInternalAPI';
-export * from './ProductAPI';
-export * from './TagInternalAPI';
--- src/lib/flotiq.ts
+++ src/lib/flotiq.ts
@@ -1,2 +1,2 @@
-import { FlotiqApi } from "../../flotiqApi/src";
+import { FlotiqApi } from "flotiq-api-ts";
 export const flotiq = new FlotiqApi(import.meta.env.FLOTIQ_API_KEY);
